fafcde4730841917176b6f042c1954f6fa7af0d2
Mholie Serrano <mholzserrano@gmail.com>
2025-04-04 15:54:47 +0800

    fix(SignUp): fixed response for submitting

--- pages/SignUp Page/assets/js/signup.js
+++ pages/SignUp Page/assets/js/signup.js
@@ -1,10 +1,8 @@
 const API_URL_SIGN_UP = "https://demo-api-skills.vercel.app/api/FinanceManager/users";
 
-// Attach event listener to the form
-document.getElementById("signUp").addEventListener("submit", function (event) {
+document.getElementById("signUp").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    // Get information from HTML inputs
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
@@ -20,14 +18,14 @@ document.getElementById("signUp").addEventListener("submit", function (event) {
     // Axios POST request to sign-up API
     axios.post(API_URL_SIGN_UP, {
         email: email,
-        name: `${firstName} ${lastName}`, // Adjusted name format
+        name: `${firstName} ${lastName}`,  // Adjusted name format
         password: password
     })
     .then(response => {
         console.log(response);
         alert("User added successfully!");
-        document.getElementById("signUp").reset(); // Reset the form
-        window.location.replace("/pages/LogIn%20Page/index.html"); // Redirect
+        document.getElementById("signUp").reset();  // Reset the form
+        window.location.replace("/pages/LogIn Page/index.html");  // Redirect
     })
     .catch(error => {
         alert("Error adding user, please check console for more information");
